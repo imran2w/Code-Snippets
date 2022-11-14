f6c96ac61382133de8c8ff5b7af883017dcbd563
ALI IMRAN <imran2w@gmail.com>
2022-11-14 23:00:37 +0600

    JS

---
+++  JavaScript/Document Ready.js
@@ -0,0 +1,9 @@
+// VANILLA plain JavaScript without jQuery
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Your code to run since DOM is loaded and ready
+});
+
+// Above is the equivalent of jQuery .ready:
+$(document).ready(function() {
+    console.log("Ready!");
+});
